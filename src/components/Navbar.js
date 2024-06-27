1c6ca08ab0f98a4f0d7cc743442b698f09803e2f
Akaah <aakashdotgupta@gmail.com>
2024-06-27 19:05:24 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ const NavBar = (props) => {
                 alt="Flowbite Logo"
               />
               <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                Flowbite
+                News App
               </span>
             </Link>
             <button
@@ -222,16 +222,6 @@ const NavBar = (props) => {
                   </div>
                 </li>
                 
-
-
-                <li>
-                  <Link
-                    to="#"
-                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                  >
-                    Pricing
-                  </Link>
-                </li>
                 <li>
                   <Link
                     to="#"
